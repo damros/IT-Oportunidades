2cd40a3769f14eb9aa144ad4bf505534412d7978
Damian - Rosario <damrosgh@gmail.com>
2017-08-22 00:21:44 -0300

    correccion al seed de usuarios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,9 +14,9 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->delete();
 
         $users = [
-            ['id' => 1, 'name' => 'Candidato', 'username' => 'candidato', 'email' => 'candidato@mail.com', 'password' => bcrypt('123'),'profile_id' => '3','activated' => '1'],
-            ['id' => 2, 'name' => 'Empresa', 'username' => 'empresa', 'email' => 'empresa@mail.com', 'password' => bcrypt('123'),'profile_id' => '4','activated' => '1'],
-            ['id' => 3, 'name' => 'Admin', 'username' => 'admin', 'email' => 'admin@mail.com', 'password' => bcrypt('123'),'profile_id' => '1','activated' => '1'],
+            ['id' => 1, 'name' => 'Candidato', 'email' => 'candidato@mail.com', 'password' => bcrypt('123'),'profile_id' => '3','activated' => '1'],
+            ['id' => 2, 'name' => 'Empresa', 'email' => 'empresa@mail.com', 'password' => bcrypt('123'),'profile_id' => '4','activated' => '1'],
+            ['id' => 3, 'name' => 'Admin', 'email' => 'admin@mail.com', 'password' => bcrypt('123'),'profile_id' => '1','activated' => '1'],
         ];
 		
 		DB::table('users')->insert($users);
