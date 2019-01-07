f3facf7c45f89b024d48e52dd7f913dad83a83ee
Damian <damrosgh@gmail.com>
2019-01-07 20:45:52 -0300

    fix de estilo
    mejora al background de la lista de candidatos para un requerimiento

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -7,79 +7,73 @@ use Redirect;
 use ITOportunidades\Http\Requests\LoginRequest;
 use ITOportunidades\Profile;
 
-class LogController extends Controller
-{
+class LogController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
+        
     }
-	
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function adminIndex()
-    {
+    public function adminIndex() {
         return view('admin.login');
     }
 
-	public function adminLogin(LoginRequest $request)
-    {
-		$profile = Profile::where('code', 'ad')->first();
-		$email = $request->input('email');
-		$password = $request->input('password');
-		
-		if (Auth::attempt(['email' => $email, 'password' => $password, 'profile_id' => $profile->id])) {
-			return redirect('/admin');
-		} else {
-			return Redirect::to('/admin/login')->withErrors("Datos incorrectos");
-		}
-		
+    public function adminLogin(LoginRequest $request) {
+        $profile = Profile::where('code', 'ad')->first();
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if (Auth::attempt(['email' => $email, 'password' => $password, 'profile_id' => $profile->id])) {
+            return redirect('/admin');
+        } else {
+            return Redirect::to('/admin/login')->withErrors("Datos incorrectos");
+        }
     }
 
-	public function adminLogout()
-    {
+    public function adminLogout() {
         Auth::logout();
-		
+
         return Redirect::to('/admin/login');
     }
-	
-	public function webLogin(LoginRequest $request)
-    {
-		$profile = Profile::where('code', 'ad')->first();
-		$email = $request->input('email');
-		$password = $request->input('password');
-		
-		if (Auth::attempt(['email' => $email, 'password' => $password]))
-		{
-		
-			if (auth()->user()->profile_id == $profile->id){
-				Auth::logout();
-				return response()->json(array(	"status"=>"error", "message"=>"Datos incorrectos"));	
-			}
-			
-			if(auth()->user()->activated == '0'){
-				Auth::logout();
-				return response()->json(array(	"status"=>"error",
-												"message"=>"Debe activar su cuenta antes de iniciar sesión",
-												"redirect"=>""));
-			}
-						
-			return response()->json(array(	"status"=>"success", "message"=>"Bienvenido ".auth()->user()->name."!"));
-		}		
 
-		return response()->json(array(	"status"=>"error", "message"=>"Datos incorrectos"));
-    }	
+    public function webLogin(LoginRequest $request) {
+        $profile = Profile::where('code', 'ad')->first();
+        $email = $request->input('email');
+        $password = $request->input('password');
 
-	public function webLogout()
-    {
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+
+            if (auth()->user()->profile_id == $profile->id) {
+                Auth::logout();
+                return response()->json(array("status" => "error", "message" => "Datos incorrectos"));
+            }
+
+            if (auth()->user()->activated == '0') {
+                Auth::logout();
+                return response()->json(array("status" => "error",
+                            "message" => "Debe activar su cuenta antes de iniciar sesión",
+                            "redirect" => ""));
+            }
+
+            return response()->json(array("status" => "success", "message" => "Bienvenido " . auth()->user()->name . "!"));
+        }
+
+        return response()->json(array("status" => "error", "message" => "Datos incorrectos"));
+    }
+
+    public function webLogout() {
         Auth::logout();
-		
+
         return Redirect::to('/');
     }
+
 }
--- resources/views/website/contact.blade.php
+++ resources/views/website/contact.blade.php
@@ -152,53 +152,6 @@
     });
 
 })(this.jQuery);
-
-/*
- var lat;
- var lng;
- var map;
- var styles = [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}];
- 
- //type your address after "address="
- jQuery.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=Av Presidente Roque Sáenz Peña 628, San Nicolás, Ciudad Autónoma de Buenos Aires&sensor=false', function(data) {
- lat = data.results[0].geometry.location.lat;
- lng = data.results[0].geometry.location.lng;
- }).complete(function(){
- dxmapLoadMap();
- });
- 
- function attachSecretMessage(marker, message)
- {
- var infowindow = new google.maps.InfoWindow(
- { content: message
- });
- google.maps.event.addListener(marker, 'click', function() {
- infowindow.open(map,marker);
- });
- }
- 
- window.dxmapLoadMap = function()
- {
- var center = new google.maps.LatLng(lat, lng);
- var settings = {
- mapTypeId: google.maps.MapTypeId.ROADMAP,
- zoom: 16,
- draggable: false,
- scrollwheel: false,
- center: center,
- styles: styles 
- };
- map = new google.maps.Map(document.getElementById('googlemaps'), settings);
- 
- var marker = new google.maps.Marker({
- position: center,
- title: '4 Latam',
- map: map
- });
- marker.setTitle('4 Latam'.toString());
- //type your map title and description here
- //attachSecretMessage(marker, '<h3>Map title</h3>Map HTML description');
- } */
 </script>
 
 @endsection
